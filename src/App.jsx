cd5d1e1ae4708af71fd8aaf27eea0f09adbfc509
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-07-06 07:32:37 +0200

    create files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+const api = {
+    key: "2ded579bd3776ca1a35191962283d553",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
