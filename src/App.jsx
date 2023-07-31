4ed4c9806e52155c86092a32e3c5c0e466f3ee2e
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-07-31 15:34:16 +0200

    Add sass + logic + data collection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import CssBaseline from '@mui/material/CssBaseline';
-import './App.css'
+import './App.scss'
 
 const api = {
     key: "2ded579bd3776ca1a35191962283d553",
@@ -8,6 +8,54 @@ const api = {
 }
 function App() {
 
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    const search = evt => {
+        if (evt.key === "Enter") {
+            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+                .then(res => res.json())
+                .then(result => {
+                    setQuery('')
+                    setWeather(result)
+                    console.log(result)
+                })
+        }
+    }
+    const dateBuilder = (d) => {
+        let months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ];
+
+        let days = [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        ];
+
+
+        let day = days[d.getDay()]
+        let date = d.getDate()
+        let month = months[d.getMonth()]
+        let year = d.getFullYear()
+
+        return `${day} ${date} ${month} ${year}`
+    }
 
   return (
     <>
@@ -18,8 +66,29 @@ function App() {
         <div className="app">
             <main>
                 <div className="search-box">
-                    <input type="text" className="search-bar" placeholder="Search..."/>
+                    <input
+                        type="text"
+                        className="search-bar"
+                        placeholder="Search..."
+                        onChange={e => setQuery(e.target.value)}
+                        value={query}
+                        onKeyPress={search}
+                    />
                 </div>
+                {(typeof weather.main != 'undefined') ? (
+                <div>
+                    <div className="location-box">
+                        <div className="location">{weather.name}, {weather.sys.country}</div>
+                        <div className="date">{dateBuilder(new Date())}</div>
+                    </div>
+                    <div className="weather-box">
+                        <div className="temp">
+                            {Math.round(weather.main.temp)}°C
+                        </div>
+                        <div className="weather">{weather.weather[0].main}</div>
+                    </div>
+                </div>
+                ) : ('')}
             </main>
         </div>
     </>
