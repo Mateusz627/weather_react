87dc29ccec481e945d893c095002a07609fc899f
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-07-31 16:03:59 +0200

    code improvement, temperature-dependent background change

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import React, {useState} from "react";
-import CssBaseline from '@mui/material/CssBaseline';
-import './App.scss'
+import React, { useState } from "react";
+import "./App.scss";
 
 const api = {
     key: "2ded579bd3776ca1a35191962283d553",
-    base: "https://api.openweathermap.org/data/2.5/"
-}
-function App() {
+    base: "https://api.openweathermap.org/data/2.5/",
+};
 
-    const [query, setQuery] = useState('');
+function App() {
+    const [query, setQuery] = useState("");
     const [weather, setWeather] = useState({});
 
-    const search = evt => {
+    const search = (evt) => {
         if (evt.key === "Enter") {
             fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-                .then(res => res.json())
-                .then(result => {
-                    setQuery('')
-                    setWeather(result)
-                    console.log(result)
+                .then((res) => {
+                    if (!res.ok) {
+                        throw new Error("City not found or API error.");
+                    }
+                    return res.json();
                 })
+                .then((result) => {
+                    setQuery("");
+                    setWeather(result);
+                })
+                .catch((error) => {
+                    console.error(error);
+                    // Tutaj możesz wyświetlić komunikat błędu na interfejsie użytkownika.
+                });
         }
-    }
+    };
+
     const dateBuilder = (d) => {
-        let months = [
+        const months = [
             "January",
             "February",
             "March",
@@ -35,64 +43,66 @@ function App() {
             "September",
             "October",
             "November",
-            "December"
+            "December",
         ];
 
-        let days = [
+        const days = [
             "Monday",
             "Tuesday",
             "Wednesday",
             "Thursday",
             "Friday",
             "Saturday",
-            "Sunday"
+            "Sunday",
         ];
 
+        const day = days[d.getDay()];
+        const date = d.getDate();
+        const month = months[d.getMonth()];
+        const year = d.getFullYear();
 
-        let day = days[d.getDay()]
-        let date = d.getDate()
-        let month = months[d.getMonth()]
-        let year = d.getFullYear()
+        return `${day} ${date} ${month} ${year}`;
+    };
 
-        return `${day} ${date} ${month} ${year}`
-    }
+    const getBackgroundClass = () => {
+        if (typeof weather.main === "undefined") return "";
+        return weather.main.temp > 16 ? "warm" : "";
+    };
 
-  return (
-    <>
-        <React.Fragment>
-            <CssBaseline />
-            {/* The rest of your application */}
-        </React.Fragment>
-        <div className="app">
+    return (
+        <div className={`app ${getBackgroundClass()}`}>
             <main>
                 <div className="search-box">
                     <input
                         type="text"
                         className="search-bar"
                         placeholder="Search..."
-                        onChange={e => setQuery(e.target.value)}
+                        onChange={(e) => setQuery(e.target.value)}
                         value={query}
                         onKeyPress={search}
                     />
                 </div>
-                {(typeof weather.main != 'undefined') ? (
-                <div>
-                    <div className="location-box">
-                        <div className="location">{weather.name}, {weather.sys.country}</div>
-                        <div className="date">{dateBuilder(new Date())}</div>
-                    </div>
-                    <div className="weather-box">
-                        <div className="temp">
-                            {Math.round(weather.main.temp)}°C
+                {typeof weather.main !== "undefined" ? (
+                    <div>
+                        <div className="location-box">
+                            <div className="location">
+                                {weather.name}, {weather.sys.country}
+                            </div>
+                            <div className="date">{dateBuilder(new Date())}</div>
                         </div>
-                        <div className="weather">{weather.weather[0].main}</div>
+                        <div className="weather-box">
+                            <div className="temp">
+                                {Math.round(weather.main.temp)}°C
+                            </div>
+                            <div className="weather">{weather.weather[0].main}</div>
+                        </div>
                     </div>
-                </div>
-                ) : ('')}
+                ) : (
+                    ""
+                )}
             </main>
         </div>
-    </>
-  )
+    );
 }
 
-export default App
+export default App;
